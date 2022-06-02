f8f1d7aa0801183b667a010ce8cfad4c4f9b01b7
SamanLotfi <sl01710@srcs.org>
2022-06-02 15:38:15 -0700

    Fixed movement

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -5,12 +5,15 @@ using UnityEngine.UI;
 
 public class PlayerControllerX : MonoBehaviour
 {
+    public GameObject plane;
     public float speed = 20.0f;
+    public float turnSpeed = 5.0f;
     public float rotationSpeed;
     public float verticalInput;
     public float horizontalInput;
-    private Text count;
     public Text countText;
+    private Text count;
+    private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +25,14 @@ public class PlayerControllerX : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        Vector3 rotOffset = player.transform.rotation * offset;
+        transform.position = player.transform.position + rotOffset;
+        transform.rotation = player.transform.rotation;
 
         // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
 
         // Rotates the car based on horizontal input
-        transform.Rotate(Vector3.up, * horizontalInput * Time.deltaTime);
+        transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime);
     }
 }
